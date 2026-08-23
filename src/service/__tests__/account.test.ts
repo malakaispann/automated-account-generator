a7535eeb699e0b6741b019fb91fda4c956053e3e
Malakai Spann <malakaispann@gmail.com>
2026-08-22 21:10:33 -0400

    test: refactor to use mocks

--- src/service/__tests__/account.test.ts
+++ src/service/__tests__/account.test.ts
@@ -1,14 +1,14 @@
-import { describe, expect, mock, test } from "bun:test";
+import { beforeEach, describe, expect, mock, test } from "bun:test";
 import { FeatureConfig } from "../../config";
 import { CreatedUser, User, type UserCreatePayload } from "../../models";
 import { type AccountApi, AccountCreationError, AccountService } from "../account";
 
 describe("Account Service", () => {
-	const FEATURE_CONFIG = FeatureConfig.readonly().parse({
+	const featureConfig = FeatureConfig.readonly().parse({
 		DRY_RUN_CREATE_ACCOUNT: "FALSE",
 	});
 
-	const USER = User.readonly().parse({
+	const user = User.readonly().parse({
 		firstName: "foo",
 		lastName: "bar",
 		primaryEmail: "f.bar@deadbeef.org",
@@ -16,64 +16,70 @@ describe("Account Service", () => {
 		organization: "/baddcafe",
 	});
 
-	const THROWING_API: AccountApi = {
-		create: (_) => {
-			throw new Error();
-		},
+	const mockCreate = mock();
+
+	const accountApi: AccountApi = {
+		create: mockCreate,
 	};
 
-	test("throws AccountCreationError when API throws", () => {
-		const service = new AccountService(THROWING_API, FEATURE_CONFIG);
-
-		expect(() => service.createUser(USER)).toThrow(AccountCreationError);
+	beforeEach(() => {
+		mock.clearAllMocks();
 	});
 
-	test("returns mock Create User when Dry Run True", () => {
-		const service = new AccountService(
-			THROWING_API,
-			FeatureConfig.readonly().parse({ DRY_RUN_CREATE_ACCOUNT: "TRUE" }),
-		);
-		const createdUser = service.createUser(USER);
-
-		expect(createdUser).toEqual(
-			expect.objectContaining({
-				...USER,
-				id: "0",
-			}),
-		);
-	});
-
-	test("creates user as expected", () => {
-		const id = "100";
-
-		const mockCreate = mock((payload: UserCreatePayload) => {
-			return CreatedUser.readonly().parse({
-				firstName: payload.name.givenName,
-				lastName: payload.name.familyName,
-				primaryEmail: payload.primaryEmail,
-				backupEmail: payload.recoveryEmail,
-				password: payload.password,
-				organization: payload.orgUnitPath,
-				id: id,
+	describe("Create User", () => {
+		test("throws AccountCreationError when API throws", () => {
+			mockCreate.mockImplementation((_: UserCreatePayload) => {
+				throw new Error();
 			});
+
+			const service = new AccountService(accountApi, featureConfig);
+			expect(() => service.createUser(user)).toThrow(AccountCreationError);
 		});
-		const copyingApi: AccountApi = {
-			create: mockCreate,
-		};
 
-		const service = new AccountService(copyingApi, FEATURE_CONFIG);
-		const createdUser = service.createUser(USER);
+		test("returns cloned user details when Dry Run True", () => {
+			const service = new AccountService(
+				accountApi,
+				FeatureConfig.readonly().parse({ DRY_RUN_CREATE_ACCOUNT: "TRUE" }),
+			);
+			const createdUser = service.createUser(user);
 
-		expect(mockCreate).toHaveBeenCalledTimes(1);
-		const mockCreatePayload = mockCreate.mock.calls[0]![0];
+			expect(mockCreate).not.toBeCalled();
+			expect(createdUser).toEqual(
+				expect.objectContaining({
+					...user,
+					id: "0",
+				}),
+			);
+		});
 
-		expect(createdUser).toEqual(
-			expect.objectContaining({
-				...USER,
-				id: id,
-				password: mockCreatePayload.password,
-			}),
-		);
-		expect(mockCreatePayload.changePasswordAtNextLogin).toBeTrue();
+		test("creates user as expected", () => {
+			const id = "100";
+
+			mockCreate.mockImplementation((payload: UserCreatePayload) => {
+				return CreatedUser.readonly().parse({
+					firstName: payload.name.givenName,
+					lastName: payload.name.familyName,
+					primaryEmail: payload.primaryEmail,
+					backupEmail: payload.recoveryEmail,
+					password: payload.password,
+					organization: payload.orgUnitPath,
+					id: id,
+				});
+			});
+			const service = new AccountService(accountApi, featureConfig);
+			const createdUser = service.createUser(user);
+
+			expect(mockCreate).toHaveBeenCalledTimes(1);
+			const mockCreatePayload = mockCreate.mock.calls[0]![0];
+
+			expect(createdUser).toEqual(
+				expect.objectContaining({
+					...user,
+					id: id,
+					password: mockCreatePayload.password,
+				}),
+			);
+			expect(mockCreatePayload.changePasswordAtNextLogin).toBeTrue();
+		});
 	});
 });
--- src/service/__tests__/extraction.test.ts
+++ src/service/__tests__/extraction.test.ts
@@ -11,95 +11,97 @@ describe("Extraction service", () => {
 	const ORGANIZATION = "/DEADBEEF";
 	const DOMAIN = "baddcafe.com";
 
-	const META_CONFIG = MetaConfig.readonly().parse({
+	const metaConfig = MetaConfig.readonly().parse({
 		DOMAIN: DOMAIN,
 		DEFAULT_ORGANIZATION: ORGANIZATION,
 	});
-	const PROMPT_CONFIG = PromptConfig.readonly().parse({
+	const promptConfig = PromptConfig.readonly().parse({
 		FIRST_NAME_PROMPT: FIRST_NAME_KEY,
 		LAST_NAME_PROMPT: LAST_NAME_KEY,
 		EMAIL_ADDRESS_PROMPT: EMAIL_ADDRESS_KEY,
 	});
 
-	const extractionService = new ExtractionService(META_CONFIG, PROMPT_CONFIG);
+	const extractionService = new ExtractionService(metaConfig, promptConfig);
 
-	test.each([
-		[[new Entry(FIRST_NAME_KEY, "a"), new Entry(LAST_NAME_KEY, "b")]],
-		[[new Entry(FIRST_NAME_KEY, "a"), new Entry(EMAIL_ADDRESS_KEY, "c")]],
-		[[new Entry(LAST_NAME_KEY, "b"), new Entry(EMAIL_ADDRESS_KEY, "c")]],
-	])("should throw EntryNotFound error when required entry not found", (entries: Entry[]) => {
-		const response = new Response(entries);
+	describe("Get User", () => {
+		test.each([
+			[[new Entry(FIRST_NAME_KEY, "a"), new Entry(LAST_NAME_KEY, "b")]],
+			[[new Entry(FIRST_NAME_KEY, "a"), new Entry(EMAIL_ADDRESS_KEY, "c")]],
+			[[new Entry(LAST_NAME_KEY, "b"), new Entry(EMAIL_ADDRESS_KEY, "c")]],
+		])("should throw EntryNotFound error when required entry not found", (entries: Entry[]) => {
+			const response = new Response(entries);
 
-		expect(() => extractionService.getUser(response)).toThrow(EntryNotFoundError);
-	});
+			expect(() => extractionService.getUser(response)).toThrow(EntryNotFoundError);
+		});
 
-	test.each([
-		[
+		test.each([
 			[
-				new Entry(FIRST_NAME_KEY, " "),
-				new Entry(LAST_NAME_KEY, "b"),
-				new Entry(EMAIL_ADDRESS_KEY, "c"),
+				[
+					new Entry(FIRST_NAME_KEY, " "),
+					new Entry(LAST_NAME_KEY, "b"),
+					new Entry(EMAIL_ADDRESS_KEY, "c"),
+				],
 			],
-		],
-		[
 			[
-				new Entry(FIRST_NAME_KEY, "a"),
-				new Entry(LAST_NAME_KEY, " "),
-				new Entry(EMAIL_ADDRESS_KEY, "c"),
+				[
+					new Entry(FIRST_NAME_KEY, "a"),
+					new Entry(LAST_NAME_KEY, " "),
+					new Entry(EMAIL_ADDRESS_KEY, "c"),
+				],
 			],
-		],
-		[
 			[
-				new Entry(FIRST_NAME_KEY, "a"),
-				new Entry(LAST_NAME_KEY, "b"),
-				new Entry(EMAIL_ADDRESS_KEY, "   "),
+				[
+					new Entry(FIRST_NAME_KEY, "a"),
+					new Entry(LAST_NAME_KEY, "b"),
+					new Entry(EMAIL_ADDRESS_KEY, "   "),
+				],
 			],
-		],
-	])("should throw EntryValidationError error when required entry blank", (entries: Entry[]) => {
-		const response = new Response(entries);
-		expect(() => extractionService.getUser(response)).toThrow(EntryValidationError);
-	});
+		])("should throw EntryValidationError error when required entry blank", (entries: Entry[]) => {
+			const response = new Response(entries);
+			expect(() => extractionService.getUser(response)).toThrow(EntryValidationError);
+		});
 
-	test.each([
-		[
+		test.each([
 			[
-				new Entry(FIRST_NAME_KEY, [""]),
-				new Entry(LAST_NAME_KEY, "b"),
-				new Entry(EMAIL_ADDRESS_KEY, "c"),
+				[
+					new Entry(FIRST_NAME_KEY, [""]),
+					new Entry(LAST_NAME_KEY, "b"),
+					new Entry(EMAIL_ADDRESS_KEY, "c"),
+				],
 			],
-		],
-		[
 			[
-				new Entry(FIRST_NAME_KEY, "a"),
-				new Entry(LAST_NAME_KEY, [[""]]),
-				new Entry(EMAIL_ADDRESS_KEY, "c"),
+				[
+					new Entry(FIRST_NAME_KEY, "a"),
+					new Entry(LAST_NAME_KEY, [[""]]),
+					new Entry(EMAIL_ADDRESS_KEY, "c"),
+				],
 			],
-		],
-		[
 			[
-				new Entry(FIRST_NAME_KEY, "a"),
-				new Entry(LAST_NAME_KEY, "b"),
-				new Entry(EMAIL_ADDRESS_KEY, [""]),
+				[
+					new Entry(FIRST_NAME_KEY, "a"),
+					new Entry(LAST_NAME_KEY, "b"),
+					new Entry(EMAIL_ADDRESS_KEY, [""]),
+				],
 			],
-		],
-	])("should throw EntryValidationError error when required not string", (entries: Entry[]) => {
-		const response = new Response(entries);
-		expect(() => extractionService.getUser(response)).toThrow(EntryValidationError);
-	});
+		])("should throw EntryValidationError error when required not string", (entries: Entry[]) => {
+			const response = new Response(entries);
+			expect(() => extractionService.getUser(response)).toThrow(EntryValidationError);
+		});
 
-	test("should create user with expected information", () => {
-		const personalEmail = "janedoe99@gmail.com";
-		const response = new Response([
-			new Entry(FIRST_NAME_KEY, "JANE"),
-			new Entry(LAST_NAME_KEY, "doE"),
-			new Entry(EMAIL_ADDRESS_KEY, personalEmail),
-		]);
+		test("should create user with expected information", () => {
+			const personalEmail = "janedoe99@gmail.com";
+			const response = new Response([
+				new Entry(FIRST_NAME_KEY, "JANE"),
+				new Entry(LAST_NAME_KEY, "doE"),
+				new Entry(EMAIL_ADDRESS_KEY, personalEmail),
+			]);
 
-		const user = extractionService.getUser(response);
-		expect(user.firstName).toBe("Jane");
-		expect(user.lastName).toBe("Doe");
-		expect(user.primaryEmail).toBe(`j.doe@${DOMAIN}`);
-		expect(user.backupEmail).toBe(personalEmail);
-		expect(user.organization).toBe(ORGANIZATION);
+			const user = extractionService.getUser(response);
+			expect(user.firstName).toBe("Jane");
+			expect(user.lastName).toBe("Doe");
+			expect(user.primaryEmail).toBe(`j.doe@${DOMAIN}`);
+			expect(user.backupEmail).toBe(personalEmail);
+			expect(user.organization).toBe(ORGANIZATION);
+		});
 	});
 });
